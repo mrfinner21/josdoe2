15c5c118f4722b4bb8cdad977f0129dbd6c2bc54
mrfinner21 <josemartinsrjr@gmail.com>
2018-06-27 21:51:10 -0300

    Add files via upload

---
+++ offline.py
@@ -0,0 +1,56 @@
+
+import requests
+import os
+import binascii
+import ecdsa
+import hashlib
+import base58
+import sys
+
+
+def privateKey(): # Generates random 256 bit private key in hex format
+    return binascii.hexlify(os.urandom(32)).decode('utf-8')
+def address(publickey): # Public Key -> Wallet Address
+    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    c = '0'; byte = '00'; zero = 0
+    var = hashlib.new('ripemd160')
+    var.update(hashlib.sha256(binascii.unhexlify(publickey.encode())).digest())
+    a = (byte + var.hexdigest())
+    doublehash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(a.encode())).digest()).hexdigest()
+    address = a + doublehash[0:8]
+    for char in address:
+        if (char != c):
+            break
+        zero += 1
+    zero = zero // 2
+    n = int(address, 16)
+    output = []
+    while (n > 0):
+        n, remainder = divmod (n, 58)
+        output.append(alphabet[remainder])
+    count = 0
+    while (count < zero):
+        output.append(alphabet[0])
+        count += 1
+    return ''.join(output[::-1])
+
+def publicKey(privatekey): # Private Key -> Public Key
+    privatekey = binascii.unhexlify(privatekey)
+    s = ecdsa.SigningKey.from_string(privatekey, curve = ecdsa.SECP256k1)
+    return '04' + binascii.hexlify(s.verifying_key.to_string()).decode('utf-8')
+
+def main():
+        n = 0 
+        while True:
+            k = privateKey()
+            if address(publicKey(k)) == "1FeexV6bAHb8ybZjqQMjJrcCrHGW9sb6uF":
+                print(k)
+                print("A TERRA   VAI EXPPLODIRRRRR")
+                e = open("vouchorar.txt", 'a')
+                e.write(k)
+                e.close()
+            n += 1
+            print(n)
+            
+if __name__ == "__main__":
+    main()
